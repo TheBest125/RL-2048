092c2f188438bf8af6803d8084eed9a2f7fc9e77
TheBest125 <kevinbruyne0125@gmail.com>
2023-07-27 02:17:58 -0700

    final game, added endgame function

--- 2048_game.py
+++ 2048_game.py
@@ -15,7 +15,7 @@ size_square = 140
 size_line = 10
 score = 0 
 
-colors = {2:( 238, 228, 218 ), 4: (237, 224, 200), 8: (242, 177, 121), 16: (242, 177, 121), 32 : (246, 124, 95),  64:(246, 94, 59), 128: (237, 207, 114), 256: (237, 204, 97), }
+colors = {2:( 238, 228, 218 ), 4: (237, 224, 200), 8: (242, 177, 121), 16: (242, 177, 121), 32 : (246, 124, 95),  64:(246, 94, 59), 128: (237, 207, 114), 256: (237, 204, 97), 512:(237, 200, 80), 1024: (237, 197, 63), 2048: (237, 194, 46)  }
 
 class tile():
     def __init__(self, num, size, value, top, left):
@@ -55,8 +55,12 @@ def draw():
     
     
     for i in tiles:
+        if tiles[i].value in colors:
+            color = colors[tiles[i].value]
+        else:
+            color = (237, 194, 46) 
         rect1 = pygame.Rect(tiles[i].top, tiles[i].left, tiles[i].size, tiles[i].size)
-        pygame.draw.rect(screen, colors[tiles[i].value], rect1)
+        pygame.draw.rect(screen, color, rect1)
         screen.blit(font3.render(str(tiles[i].value), True, (50,50,50)), (tiles[i].top + size_square/3 - 15, tiles[i].left + size_square/3 - 10 ))
             
     
@@ -228,7 +232,7 @@ def lose():
            check = [i + 1, i - 1, i + 4, i - 4]
 
         check = [ x for x in check if (x > -1 and x < 16)]
-        
+        print(check)
         for x in check:
            if tiles[i].value == tiles[x].value:
                 return False
@@ -251,93 +255,105 @@ tiles[num2] = tile(num2, size_square, val2, start_L + (size_square + size_line) 
 
 
 
+def wait():
+    draw()
+    pygame.display.flip()
+    pygame.time.wait(50)
+
+
+def endgame():
+    rect1 = pygame.Rect(start_L - 25, start_T - 25, size_square * 4 + size_line * 3 + 50, size_square * 4 + size_line * 3 + 50)
+    pygame.draw.rect(screen, (80,80,80), rect1)
+    screen.blit(font.render("Game Over", True, (50,50,50)), (start_L + 50,  start_T + 200))
+    
+    
 
 
 
-
-
-
-
-
+gameover = False
 
 while running:
   
-      
+    if not gameover:
         
-    for event in pygame.event.get():
-        
-        if event.type == pygame.QUIT:
-            running = False
+        for event in pygame.event.get():
+            
+            if event.type == pygame.QUIT:
+                running = False
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                res = moveright()
-                draw()
-                pygame.display.flip()
-                pygame.time.wait(200)
-                if res:
-                    avail = [i for i in range(16) if i not in tiles]
-                    num1 = random.choice(avail)
-                    tiles[num1] = tile(num1, size_square, random.choices((2,4),[9,1])[0], start_L + (size_square + size_line) * (num1 % 4), start_T + (size_square + size_line) * (num1 // 4))
-                    draw()
-                    if len(avail) == 0 and lose():
-                        break
-                else:
-                    continue
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    res = moveright()
+                    wait()
+                    if res:
+                        avail = [i for i in range(16) if i not in tiles]
+                        num1 = random.choice(avail)
+                        tiles[num1] = tile(num1, size_square, random.choices((2,4),[9,1])[0], start_L + (size_square + size_line) * (num1 % 4), start_T + (size_square + size_line) * (num1 // 4))
+                        draw()
+                        if len(avail) == 1 and lose():
+                            gameover = True
+                    else:
+                        continue
 
-            elif event.key == pygame.K_LEFT:
-                res = moveleft()
-                draw()
-                pygame.display.flip()
-                pygame.time.wait(200)
-                if res:
-                    avail = [i for i in range(16) if i not in tiles]
-                    num1 = random.choice(avail)
-                    tiles[num1] = tile(num1, size_square, random.choices((2,4),[9,1])[0], start_L + (size_square + size_line) * (num1 % 4), start_T + (size_square + size_line) * (num1 // 4))
-                    draw()
-                    if len(avail) == 0 and lose():
-                        break
-                else:
-                    continue
-                
+                elif event.key == pygame.K_LEFT:
+                    res = moveleft()
+                    wait()
+                    if res:
+                        avail = [i for i in range(16) if i not in tiles]
+                        num1 = random.choice(avail)
+                        tiles[num1] = tile(num1, size_square, random.choices((2,4),[9,1])[0], start_L + (size_square + size_line) * (num1 % 4), start_T + (size_square + size_line) * (num1 // 4))
+                        draw()
+                        if len(avail) == 1 and lose():
+                            gameover = True
+                    else:
+                        continue
+                    
 
-            elif event.key == pygame.K_UP:
-                res = moveup()
-                draw()
-                pygame.display.flip()
-                pygame.time.wait(200)
-                if res:
-                    avail = [i for i in range(16) if i not in tiles]
-                    num1 = random.choice(avail)
-                    tiles[num1]= tile(num1, size_square, random.choices((2,4),[9,1])[0], start_L + (size_square + size_line) * (num1 % 4), start_T + (size_square + size_line) * (num1 // 4))
-                    draw()
-                    if len(avail) == 0 and lose():
-                        break
-                else:
-                    continue
+                elif event.key == pygame.K_UP:
+                    res = moveup()
+                    wait()
+                    if res:
+                        avail = [i for i in range(16) if i not in tiles]
+                        num1 = random.choice(avail)
+                        tiles[num1]= tile(num1, size_square, random.choices((2,4),[9,1])[0], start_L + (size_square + size_line) * (num1 % 4), start_T + (size_square + size_line) * (num1 // 4))
+                        draw()
+                        if len(avail) == 1 and lose():
+                            gameover = True
+                    else:
+                        continue
 
-            elif event.key == pygame.K_DOWN:
-                res = movedown()
-                draw()
-                pygame.display.flip()
-                pygame.time.wait(200)
-                if res:
-                    avail = [i for i in range(16) if i not in tiles]
-                    num1 = random.choice(avail)
-                    tiles[num1]= tile(num1, size_square, random.choices((2,4),[9,1])[0], start_L + (size_square + size_line) * (num1 % 4), start_T + (size_square + size_line) * (num1 // 4))
-                    draw()
-                    if len(avail) == 0 and lose():
-                        break
-                else:
-                    continue
+                elif event.key == pygame.K_DOWN:
+                    res = movedown()
+                    wait()
+                    if res:
+                        avail = [i for i in range(16) if i not in tiles]
+                        num1 = random.choice(avail)
+                        tiles[num1]= tile(num1, size_square, random.choices((2,4),[9,1])[0], start_L + (size_square + size_line) * (num1 % 4), start_T + (size_square + size_line) * (num1 // 4))
+                        draw()
+
+                    else:
+                        if len(avail) == 1 and lose():
+                            gameover = True
+                        else:
+                            continue
+
+    else:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
     
     
+
 
     
     clock.tick(60)
+    
     draw()
+    if gameover:
+        endgame()
     pygame.display.flip()
+
     
 pygame.quit()
 
